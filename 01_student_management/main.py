0ba5606d5caa332a815de6d446b38c3d96197f18
May-tea <mitir79@gmail.com>
2026-07-15 18:45:46 +0330

    feat: add main menu

--- 01_student_management/main.py
+++ 01_student_management/main.py
@@ -1,4 +1,14 @@
 def main():
-    ...
-    
+    choice = int(
+        input(
+            "1. Add Student\n"
+            "2. Show Students\n"
+            "3. Search Student\n"
+            "4. Delete Student\n"
+            "5. Exit\n\n"
+            "Enter your choice: "
+        )
+    )
+
+
 main()
