0e3754b006c255f6eff5f660b4886ec132c7d291
May-tea <mitir79@gmail.com>
2026-07-17 09:00:38 +0330

    test: add tests for name validation

--- 01_student_management/tests/test_student.py
+++ 01_student_management/tests/test_student.py
@@ -1,4 +1,4 @@
-from student import create_student
+from student import create_student, validate_name
 
 
 def test_create_student():
@@ -7,3 +7,12 @@ def test_create_student():
     assert student["name"] == "Ali"
     assert student["age"] == 20
     assert student["score"] == 90
+
+def test_validate_name():
+    assert validate_name("Ali")
+    assert validate_name("Ali Rezaei")
+    
+    assert not validate_name("A")
+    assert not validate_name("Ali123")
+    assert not validate_name("")
+    assert not validate_name(" ")
