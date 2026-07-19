f3bbf425399082d4bbdb03da3a3398db29670f0c
May-tea <mitir79@gmail.com>
2026-07-19 05:06:33 +0330

    feat: add operator selection

--- 02_calculator/main.py
+++ 02_calculator/main.py
@@ -2,9 +2,12 @@ def main():
     number1 = float(input("Enter your first number: "))
     number2 = float(input("Enter your second number: "))
     
+    operator = input("Select an operator (+   -   *   /): ")
+        
     print(
         f"First number: {number1:g}\n"
-        f"Second number: {number2:g}"
+        f"Second number: {number2:g}\n"
+        f"Operator: {operator}\n"
     )
 
 
