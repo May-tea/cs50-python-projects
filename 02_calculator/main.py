3e57c2eb9dd457f013b0a48297509691d75b6da5
May-tea <mitir79@gmail.com>
2026-07-19 05:03:40 +0330

    feat: add user input for two numbers

--- 02_calculator/main.py
+++ 02_calculator/main.py
@@ -1,4 +1,11 @@
-def main(): ...
+def main():
+    number1 = float(input("Enter your first number: "))
+    number2 = float(input("Enter your second number: "))
+    
+    print(
+        f"First number: {number1:g}\n"
+        f"Second number: {number2:g}"
+    )
 
 
 main()
