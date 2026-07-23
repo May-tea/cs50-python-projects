a02864b07fb736d038cf1b93714b5c314bd2dbf3
May-tea <mitir79@gmail.com>
2026-07-23 08:28:34 +0330

    feat: add password input function

--- 03_password_manager/main.py
+++ 03_password_manager/main.py
@@ -2,8 +2,13 @@ def get_username() -> str:
     return input("Enter your username: ").strip()
 
 
+def get_password() -> str:
+    return input("Enter your password: ").strip()
+
+
 def main() -> None:
     username: str = get_username()
+    password: str = get_password()
 
 
 if __name__ == "__main__":
