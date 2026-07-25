5606293750e327664471f23590036d41345c4f54
May-tea <mitir79@gmail.com>
2026-07-25 10:33:22 +0330

    feat: create main menu for contacts book

--- 04_contact_book/main.py
+++ 04_contact_book/main.py
@@ -1,3 +1,31 @@
-def main(): ...
+def main():
+    while True:
+        print("""
+1. Add
+2. Edit
+3. Delete
+4. Search
+5. Show All
+6. Exit
+""")
+        choice = input("Choose: ")
 
-main()
+        match choice:
+            case "1":
+                print("Add")
+            case "2":
+                print("Edit")
+            case "3":
+                print("Delete")
+            case "4":
+                print("Search")
+            case "5":
+                print("Show All")
+            case "6":
+                break
+            case _:
+                print("Invalid choice.")
+
+
+if __name__ == "__main__":
+    main()
