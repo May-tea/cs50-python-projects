4f9fb259f7e895d9c92aa6ef19b400713556ce2a
May-tea <mitir79@gmail.com>
2026-07-28 02:07:10 +0330

    feat: implement expense creation with input validation

--- 05_expense_tracker/main.py
+++ 05_expense_tracker/main.py
@@ -1,4 +1,59 @@
-def main(): ...
+from datetime import datetime
+
+Expense = dict[str, str | float]
+
+
+def create_expense(title: str, amount: float, date: str) -> Expense:
+    return {"title": title, "amount": amount, "date": date}
+
+
+def add_expense(expenses: list[Expense]) -> None:
+    while True:
+        title: str = input("Enter Expense Title: ").strip()
+
+        if not title:
+            print("Title cannot be empty.")
+            continue
+
+        if len(title) < 3:
+            print("Title must be at least 3 characters long.")
+            continue
+
+        break
+
+    while True:
+        try:
+            amount: float = float(input("Enter Expense Amount: "))
+
+            if amount <= 0:
+                print("Amount must be greater than 0.")
+                continue
+
+            break
+        except ValueError:
+            print("Invalid amount. Please enter a valid number.")
+            continue
+
+    while True:
+        try:
+            expense_date: str = input("Enter expense date (YYYY-MM-DD): ")
+
+            datetime.strptime(expense_date, "%Y-%m-%d")
+
+            break
+        except ValueError:
+            print("Invalid date. Please enter a valid date.")
+            continue
+
+    expenses.append(create_expense(title, amount, expense_date))
+
+    print("Expense added successfully.")
+
+
+def main() -> None:
+    expenses: list[Expense] = []
+
+    add_expense(expenses)
 
 
 main()
