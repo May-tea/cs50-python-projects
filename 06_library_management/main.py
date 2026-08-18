71f5ceb42f040bb9a6f79041a964554290d07d8c
May-tea <mitir79@gmail.com>
2026-08-18 09:25:09 +0330

    feat: add book creation and validation

--- 06_library_management/main.py
+++ 06_library_management/main.py
@@ -1,4 +1,42 @@
-def main(): ...
+Book = dict[str, str | int]
+
+
+def add_book(books: list[Book]) -> None:
+    while True:
+        title: str = input("Enter book title: ").strip().lower()
+
+        if not title:
+            print("\nTitle can not be empty.\n")
+            continue
+
+        author: str = input("Enter author: ").strip().lower()
+
+        if not author:
+            print("\nAuthor can not be empty.\n")
+            continue
+
+        try:
+            year: int = int(input("Enter publication year: ").strip())
+        except ValueError:
+            print("\nPlease enter a valid publication year for the book.\n")
+            continue
+
+        break
+
+    new_book = create_book(title, author, year)
+
+    books.append(new_book)
+
+
+def create_book(title: str, author: str, year: int) -> Book:
+    return {"title": title, "author": author, "year": year}
+
+
+def main() -> None:
+    books: list[Book] = []
+
+    add_book(books)
+    print("\nBook added successfully.")
 
 
 main()
