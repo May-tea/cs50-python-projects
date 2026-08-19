1296b5c6a7cc1d9a2bdb74a0c7ff0bcf734428c7
May-tea <mitir79@gmail.com>
2026-08-19 11:56:04 +0330

    feat: add Student class with basic attributes

---
+++ 07_final_project/student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name: str, age: int, email: str, score: float) -> None:
+        self.name = name
+        self.age = age
+        self.email = email
+        self.score = score
