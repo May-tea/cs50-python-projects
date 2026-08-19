3500fd4eebefb4ef32ee777b8619c2a73c7beaa2
May-tea <mitir79@gmail.com>
2026-08-19 13:44:21 +0330

    feat: add function to save students to CSV

--- 07_final_project/src/csv_handler.py
+++ 07_final_project/src/csv_handler.py
@@ -1,1 +1,12 @@
 import csv
+
+from student import Student
+
+
+def save_to_csv(students: list[Student], filename: str) -> None:
+    with open(filename, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["name", "age", "email", "score"])
+
+        for student in students:
+            writer.writerow([student.name, student.age, student.email, student.score])
