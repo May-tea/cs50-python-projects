0c8419c5388e6b8a5e14ff72e4d9253871f217d9
May-tea <mitir79@gmail.com>
2026-08-19 13:51:40 +0330

    feat: add function to load students from CSV

--- 07_final_project/src/csv_handler.py
+++ 07_final_project/src/csv_handler.py
@@ -2,11 +2,27 @@ import csv
 
 from student import Student
 
+FIELD_NAMES: list[str] = ["name", "age", "email", "score"]
+
 
 def save_to_csv(students: list[Student], filename: str) -> None:
     with open(filename, "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow(["name", "age", "email", "score"])
+        writer.writerow(FIELD_NAMES)
 
         for student in students:
             writer.writerow([student.name, student.age, student.email, student.score])
+
+
+def load_from_csv(filename: str) -> list[Student]:
+    with open(filename, newline="", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+
+        students: list[Student] = []
+
+        for row in reader:
+            students.append(
+                Student(row["name"], int(row["age"]), row["email"], float(row["score"]))
+            )
+
+        return students
