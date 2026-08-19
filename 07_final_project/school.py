7674e456d2b7bb2e7e1b1f7858a267bc5de96d94
May-tea <mitir79@gmail.com>
2026-08-19 12:03:42 +0330

    feat: add method to register students

--- 07_final_project/school.py
+++ 07_final_project/school.py
@@ -4,3 +4,6 @@ from student import Student
 class School:
     def __init__(self) -> None:
         self.students: list[Student] = []
+
+    def add_student(self, student: Student) -> None:
+        self.students.append(student)
