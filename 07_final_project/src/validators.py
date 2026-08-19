c1788cdd7a0ac00a913c82c87534d1ea5c38bf36
May-tea <mitir79@gmail.com>
2026-08-19 16:45:56 +0330

    feat: add input validation functions

--- 07_final_project/src/validators.py
+++ 07_final_project/src/validators.py
@@ -5,3 +5,15 @@ def validate_email(email: str) -> bool:
     pattern = r"^[\w\.-]+@[\w\.]+\.\w+$"
 
     return re.match(pattern, email) is not None
+
+
+def validate_name(name: str) -> bool:
+    return name.replace(" ", "").isalpha() and len(name) >= 3
+
+
+def validate_age(age: int) -> bool:
+    return 1 <= age <= 119
+
+
+def validate_score(score: float) -> bool:
+    return 0 <= score <= 20
